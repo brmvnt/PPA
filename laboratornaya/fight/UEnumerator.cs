b62b8c68cd871edc5e697fd27d087f07377d1133
Setuyaa <101656452+Setuyaa@users.noreply.github.com>
2023-04-18 22:37:09 +0300

    Update UEnumerator.cs

--- laboratornaya/fight/UEnumerator.cs
+++ laboratornaya/fight/UEnumerator.cs
@@ -1,12 +1,13 @@
 using System;
 namespace age_of_war
 {
-	public enum UEnumerator
-	{
-		LightInfantry = 1,
-		HeavyInfantry = 2,
-		Knight = 3,
-		Archery = 4,
-		Healer = 5
-	}
+    public enum UEnumerator
+    {
+        LightInfantry = 1,
+        HeavyInfantry = 2,
+        Knight = 3,
+        Archery = 4,
+        Healer = 5,
+        Cloner = 6
+    }
 }
