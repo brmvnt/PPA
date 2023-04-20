00d3c7f51f7c16bf805f73ed1edd077dae0f9f5f
Setuyaa <101656452+Setuyaa@users.noreply.github.com>
2023-04-20 18:12:07 +0300

    Update Arrow.cs

--- laboratornaya/fight/Arrow.cs
+++ laboratornaya/fight/Arrow.cs
@@ -5,13 +5,14 @@ namespace age_of_war
     {
         public SAEnumeration Ability { get; }
         public int power = 5;
-        public int range = 3;
+        public int range { get; }
         public Arrow()
         {
             constHp = 10;
             hp = constHp;
             attack = 5;
             defence = 5;
+            range = 3;
             Ability = SAEnumeration.Arrow;
             cost = attack + defence + hp + (power + range) * 2;
         }
@@ -19,13 +20,26 @@ namespace age_of_war
         public new int Cost
         {
             get { return cost; }
-            set { cost = attack + defence + hp + (power + range)*2; }
+            set { cost = attack + defence + hp + (power + range) * 2; }
         }
 
         public string name = "Arrow";
         public override string ToString()
         {
             return $"{name}";
+        }
+        public void Action(ISpecialAbility arrow1, Army army1, Army army2, int i, int j)
+        {
+            Arrow arrow = (Arrow)arrow1;
+            Console.WriteLine("im here arrow");
+            Action act = new Action();
+            Game game = new Game();
+            if (army2.army.Count() > 0)
+            {
+                if (army2.army[0].IsStillAlive())
+                    army2.army[0].GetHit(arrow.power, game.ArmyPrice);
+                act.Death(i, arrow, army2.army[0], army2, army1, j);
+            }
         }
     }
 }
