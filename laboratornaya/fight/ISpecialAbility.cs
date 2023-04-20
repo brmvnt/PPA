46050e2a0d96a83ec9ec993388b5201c9c6b69a5
Setuyaa <101656452+Setuyaa@users.noreply.github.com>
2023-04-20 18:15:30 +0300

    Update ISpecialAbility.cs

--- laboratornaya/fight/ISpecialAbility.cs
+++ laboratornaya/fight/ISpecialAbility.cs
@@ -4,5 +4,7 @@ namespace age_of_war
     public interface ISpecialAbility
     {
         public SAEnumeration Ability { get; }
+        public int range { get; }
+        public void Action(ISpecialAbility special, Army army1, Army army2, int i, int j) { }
     }
 }
