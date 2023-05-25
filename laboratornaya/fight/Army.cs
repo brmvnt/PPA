d9e5531c15803e792c3b9cd2a0ab85342ad3b3d1
Setuyaa <101656452+Setuyaa@users.noreply.github.com>
2023-05-25 15:10:00 +0300

    Update Army.cs

--- laboratornaya/fight/Army.cs
+++ laboratornaya/fight/Army.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +8,29 @@ namespace age_of_war
 {
     public class Army
     {
-        public List<Unit> army;
+        public List<Proxy> army;
         public string name;
-        public Army(string name)
-        { 
-            
+        public Army(string name, int i)
+        {
             this.name = name;
-            if (name == "Computer")
+            if (i == 1)
             {
-                var hf = new HIFactory();
-                var hi = hf.Create();
-                var kf = new KFactory();
-                var k = kf.Create();
-                army = new List<Unit>() { hi, k }; 
+                if (name == "Computer")
+                {
+                    var hf = new HIFactory();
+                    Proxy pr = new Proxy(hf.Create());
+                    var kf = new KFactory();
+                    Proxy pr0 = new Proxy(kf.Create());
+                    var lf = new LIFactory();
+                    Proxy pr1 = new Proxy(lf.Create());
+                    Proxy pr2 = new Proxy(lf.Create());
+                    Proxy pr3 = new Proxy(lf.Create());
+                    Proxy pr4 = new Proxy(lf.Create());
+                    army = new List<Proxy>() {pr, pr0, pr4, pr2 };
+                }
+                else
+                    army = BuyArmy.BuyArmyMain();
             }
-            else
-                army = BuyArmy.BuyArmyMain();
         }
         public override string ToString()
         {
